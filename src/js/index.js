58c1e92f25c67271b58e1ab2ac9c02ed93ba3458
Ben Kulka <btkulka@gmail.com>
2019-04-29 15:32:43 -0600

    setting up ui
    adding layout, fonts, and custom css

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,24 @@
-import $ from jQuery;
+// static variables ============================
+var FEED = [];
+
+// static functions ============================
+function addToFeed(message, type = "basic"){
+
+    var bullet = "<li>" + Date.getTime() + ": " + message + "</li>";
+
+    if(type == "error"){
+        bullet.addClass("text-danger");
+    }
+
+    $("#feed-list").append(bullet)
+}
+
+// logic =======================================
 
 // Initialize
 $(document).ready(function(){
+
+    // Begin list
+   addToFeed("Began application. Good luck!");
 
 });
