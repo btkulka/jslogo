61f4cac53fd9d688cbd53501078ec1e3246b28a5
Ben Kulka <btkulka@gmail.com>
2019-05-01 20:55:55 -0600

    basic solution completed!
    should be good to go. did quite a bit of testing on it. debating on tackling the canvas challenge or not-- sitting at ~5hrs right now on this

--- src/js/classes.js
+++ src/js/classes.js
@@ -1,28 +1,38 @@
 class Dot {
 
-    constructor(c, src, s){
+    constructor(i, c, src, s){
+        this.id = i;            // a unique identifier for the dot
         this.color = c;         // the dot's color
         this.img_src = src;     // the url for the dot
         this.size = s;          // number of pixels in length of the dot image
     }
 
-    print(size){
-        return "<img class='data_dot' src='" + this.img_src + "' style='height:" + this.size + "px; width:" + this.size + "px;'/>";
+    print(){
+        return  "<img class='data_dot' "
+                + "id='" + this.id + "' "
+                + "src='" + this.img_src + "' " 
+                + "style='height:" + this.size + "px; width:" + this.size + "px;' "
+                + "ondragstart='handleDrag(event, \"" + this.img_src+ "\"," + this.size + ", \"" + this.color + "\", \"" + this.id + "\")'"
+                + "draggable='true'/>";
     }
-
 }
 
 class Slot {
 
-    constructor(ratio, c, posX, posY){
-        this.ratio = ratio;             // ratio of the size of the current image vs the original
+    constructor(ratio, c, posX, posY, s){
+        this.ratio = ratio;             // ratio of the size of the current image instance vs the original
         this.color = c;                 // color of the dot required
         this.x = posX * ratio;          // x-coordinate
         this.y = posY * ratio;          // y-coordinate
+        this.size = s;                  // size of dots in the current instance
     }
 
     print(){
-        return "<div class='data_slot' style='position: absolute; margin-left: " + this.x + "px; margin-top: " + this.y + "px; z-index: 1;'></div>";
+        return  "<div class='data_slot' "
+                + "ondragover='handleDragOver(event)'"
+                + "ondrop='handleDrop(event, \"" + this.color + "\")'"
+                + "style='position: absolute; margin-left: " + this.x + "px; margin-top: " + this.y + "px; z-index: 1;"
+                + "height: " + this.size + "; width: " + this.size + "'></div>";
     }
 
 }
--- src/js/index.js
+++ src/js/index.js
@@ -10,16 +10,17 @@ var CONFIG = {                                             // all hardcoded valu
         {"color":"red", "src": "../imgs/ia-logo-dot-red.png"}
     ],
     "slots": [
-        {"x": 190, "y": 383, "color": "green"},
-        {"x": 90, "y": 237, "color": "black"},
-        {"x": 500, "y": 115, "color": "red"},
-        {"x": 463, "y": 333, "color": "black"},
-        {"x": 320, "y": 95, "color": "blue"}
+        {"x": 150, "y": 343, "color": "green"},
+        {"x": 55, "y": 197, "color": "black"},
+        {"x": 465, "y": 80, "color": "red"},
+        {"x": 423, "y": 293, "color": "black"},
+        {"x": 280, "y": 55, "color": "blue"}
     ]
 };
 
 var DOTS = [];                                              // our array of Dot objects
 var SLOTS = [];                                             // our array of Slot objects
+var CORRECT_COUNT = 0;                                      // running count of the correct placements
 
 // UI functionality ============================
 
@@ -40,6 +41,7 @@ function SwapViews(pill){
                 .addClass("d-flex")
                 .removeClass("d-none")
                 .fadeIn();
+            addToFeed("Swapping to basic solution...");
         }else{
             $("#basic-pill").removeClass("active");
             $("#basicCol")
@@ -50,6 +52,7 @@ function SwapViews(pill){
                 .addClass("d-flex")
                 .removeClass("d-none")
                 .fadeIn();
+            addToFeed("Swapping to canvas solution...");
         }
         navlink.addClass("active");
     }
@@ -57,22 +60,77 @@ function SwapViews(pill){
 
 // static functions ============================
 
-function addToFeed(message, type = "basic"){
+// Resets the Basic Workspace to its starting state
+function reset(){
+    addToFeed("Returning dots to their starting position...");
+    DOTS = [];
+    SLOTS = [];
+    CORRECT_COUNT = 0;
+    $("#basicWorkspace").empty();
+    loadImages();
+}
 
-    var bullet = "<li>" + Date.getTime() + ": " + message + "</li>";
+// adds a message to the feed. Passing "error" as second param paints text red
+function addToFeed(message, type = "basic"){
+    var bullet = $("<li>" + message + "</li>");
 
     if(type == "error"){
         bullet.addClass("text-danger");
     }
 
-    $("#feed-list").append(bullet)
+    $("#feed-list").prepend(bullet)
 }
 
-// logic =======================================
+// handles drag on all Dot elements
+function handleDrag(ev, src, size, color, id){
+    ev.dataTransfer.setData("text/plain", JSON.stringify ({"color": color, "id": id}));
+    ev.dataTransfer.setDragImage(new Image(src), size, size);
+    ev.dataTransfer.dropEffect = "move";
+}
 
-// Initialize
-$(document).ready(function(){
+// handles dragover on all Slot element
+function handleDragOver(ev){
+    ev.preventDefault();
+    ev.dataTransfer.dropEffect = "move"
+}
 
+// handles drop on all Slot elements
+function handleDrop(ev, color){
+    ev.preventDefault();
+
+    var td = JSON.parse(ev.dataTransfer.getData("text/plain"));
+    try{
+        if(color == td.color){
+            if($(ev.target).children().length > 0 || $(ev.target).hasClass("data_dot")){
+                throw "There's already a dot on this slot!"
+            }else{
+                CORRECT_COUNT = CORRECT_COUNT + 1;            
+                var dot = document.getElementById(td.id);
+                dot.draggable = false;
+                ev.target.append(dot);
+
+                if(CORRECT_COUNT == 5){
+                    win();
+                }else{
+                    addToFeed("Correct! " + (5 - CORRECT_COUNT) + " dots left to go!");
+                }
+            }
+        }else{
+            throw "Wrong color dot for this slot!";
+        }
+    }catch(ex){
+        reject(ex);
+    }
+}
+
+// Occurs when a Dot is not on the right Slot
+// creating as a function in case I want to expand functionality
+function reject(ex){
+    addToFeed(ex, "error");
+}
+
+// only called at the beginning of the app, used to set CONFIG values
+function initialize(){
     var workspace = $("#basicWorkspace");
 
     // calculate space
@@ -82,20 +140,33 @@ $(document).ready(function(){
     CONFIG.dotSize = CONFIG.logoSize * (86 / 600);    // ratio of dots to logo currently
     CONFIG.ratio = CONFIG.logoSize / 600;
 
+    loadImages();
+}
+
+// loads images into the basic workspace and populates global data
+function loadImages(){
+    var workspace = $("#basicWorkspace");
     var dotShelf = $("<div class='row d-flex justify-content-between' style='min-width:" + CONFIG.logoSize + "px; max-width:" + CONFIG.logoSize + "px'></div>");
     workspace.append(dotShelf);
     
+    // increment this and assign to each dot
+    var id = 1;
+
     // Load images
     $(CONFIG.dots).each(function(){
 
-        var d = new Dot(this.color, this.src, CONFIG.dotSize);
+        var d = new Dot("dot"+id, this.color, this.src, CONFIG.dotSize);
+        
+        id = id + 1;
 
-        dotShelf.append(d.print(CONFIG.dotSize));
+        dotShelf.append(d.print());
         DOTS.push(d);
 
         // we want two black dots, do this twice
         if(this.color == "black"){
-            dotShelf.append(d.print(CONFIG.dotSize));
+            d = new Dot("dot"+id, this.color, this.src, CONFIG.dotSize);
+            id = id + 1;
+            dotShelf.append(d.print());
             DOTS.push(d);
         }
         
@@ -107,9 +178,24 @@ $(document).ready(function(){
 
     // append slots
     $(CONFIG.slots).each(function(){
-        var s = new Slot(CONFIG.ratio, this.color, this.x, this.y);
+        var s = new Slot(CONFIG.ratio, this.color, this.x, this.y, CONFIG.dotSize);
         SLOTS.push(s);
         logoWrapper.append(s.print());
     });
 
+    addToFeed("Move all dots to their correct position. Good luck!");
+}
+
+// Initialize
+$(document).ready(function(){
+    initialize();
 });
+
+function ClaimPrize(){
+    window.location.href = "https://www.youtube.com/watch?v=oHg5SJYRHA0";
+}
+
+function win(){
+    addToFeed("You win! Congratulations!");
+    $("#winModal").modal("show");
+}
